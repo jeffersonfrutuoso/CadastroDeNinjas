ee2d5b1b6a6acfe38bfdcccd374b0cfd3a008764
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-06-09 10:29:17 -0300

    criando a primera rota

---
+++ CadastroDeNinjas/src/main/java/frutuoso/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package frutuoso.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Meu primeiro programa em java, me sinto sujo";
+    }
+}
