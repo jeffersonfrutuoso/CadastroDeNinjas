5e82c78c844a04ca57d8d6feefaf40b8a7832d7e
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-06-11 19:33:34 -0300

    feat: cria os endpoints

--- CadastroDeNinjas/src/main/java/frutuoso/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/frutuoso/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package frutuoso.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,30 @@ public class NinjaController {
     public String boasVindas() {
         return "Meu primeiro programa em java, me sinto sujo";
     }
+
+    @GetMapping("/mostrarTodos")
+    public String mostrarTodos() {
+        return "mostrando todos";
+    }
+
+    @GetMapping("/mostrarID")
+    public String mostrarPorId() {
+        return "mostrando por id";
+    }
+
+    @PostMapping("/Create")
+    public String Criando() {
+        return "criando";
+    }
+
+    @PutMapping("/Atualizar")
+    public String Atualizando() {
+        return "Atualizar";
+    }
+
+    @DeleteMapping("/Deletar")
+    public String Deletar() {
+        return "Delatando";
+    }
+
 }
